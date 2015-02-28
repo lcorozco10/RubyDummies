3a285bb344315a5bf94253d1455d806e8a84b338
lcorozco10 <lcorozco10@gmail.com>
2015-02-28 10:33:31 -0600

    Clase 1 de Ruby

---
+++ ruby1.rb
@@ -0,0 +1,48 @@
+=begin
+
+  Autor:        Luis Orozco
+  Fecha:        27-02-2015
+  descripcion:  Aprendiendo Ruby
+
+=end
+
+#Variable global
+$var1 = "Variable Global"
+
+
+class MainClass
+  #Contructor principal de la clase MainClass
+  def initialize(name)
+
+    #Variable de instacia
+    @var2 = 'variable de instancia'
+    @var = 'Hola '+name+"..! Tu nombre contiene " +name.length.to_s+" caracteres\n"
+    print @var
+
+  end
+
+  #Declaracion de un metodo
+  def calculador(x,y)
+    #asignacion y operacion matematicas
+    z = ((y+(50-40)/x)/x**3)
+    return 'Esta es una operacion aritmetica "Ruby" = '+z.to_s()
+  end
+
+  def variables()
+    var3 = "variable local"
+    puts $var1
+    puts @var2
+    puts var3
+  end
+
+  #Ejemplo de bucles en ruby
+  def bucles()
+    puts "*-"*25
+  end
+end
+print 'Nombre: '
+nombre = gets().chomp()
+obj = MainClass.new(nombre)
+puts obj.calculador("2".to_i,4.to_f)
+obj.variables()
+obj.bucles
